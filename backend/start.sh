c08a6fcbd69c916a48c32f212f7ecc1a52425133
arensusu <willy9966@gmail.com>
2024-05-01 08:12:21 +0000

    modify api start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -3,6 +3,6 @@ set -e
 
 source .env
 
-/app/migrate -path /app/migration -database "postgres://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOST:5432/$DATABASE_NAME?sslmode=disable" -verbose up
+/app/migrate -path /app/migration -database "postgres://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME?sslmode=disable" -verbose up
 
 exec "$@"
