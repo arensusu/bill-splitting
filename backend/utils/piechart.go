684bac96a9823f3b22ab46369ae8ff306d1640b6
arensusu <willy9966@gmail.com>
2024-09-10 09:49:58 +0000

    update chart

--- backend/utils/piechart.go
+++ backend/utils/piechart.go
@@ -18,14 +18,22 @@ var chartTml = `{{- define "chart" }}
     {{- template "header" . }}
 <body>
     {{- template "base" . }}
-	<div class="container">
-		<table class="table table-striped">
+	<div class="container-md">
+		<span class="px-5">Filters:</span>
+		<div class="btn-group" role="group" aria-label="button group">
+			{{range (index .MultiSeries 0).Data}}
+				<button type="button" class="btn" data-bs-toggle="button">{{.Name}}</button>
+			{{end}}
+		</div>
+	</div>
+	<div class="container-md overflow-y-auto" style="max-height: 600px">
+		<table class="table" id="sortTable">
 			<thead>
 				<tr>
-					<th>Date</th>
-					<th>Category</th>
-					<th>Description</th>
-					<th>Price</th>
+					<th class="position-relative" onclick="sortTable(0)">Date <span class="position-absolute end-0">&#x25b4;&#x25be;</span></th>
+					<th class="position-relative" onclick="sortTable(1)">Category <span class="position-absolute end-0">&#x25b4;&#x25be;</span></th>
+					<th class="position-relative" onclick="sortTable(2)">Description <span class="position-absolute end-0">&#x25b4;&#x25be;</span></th>
+					<th class="position-relative" onclick="sortTable(3)">Price <span class="position-absolute end-0">&#x25b4;&#x25be;</span></th>
 				</tr>
 			</thead>
 			<tbody>
@@ -43,6 +51,101 @@ var chartTml = `{{- define "chart" }}
     .container {margin-top:30px; display: flex;justify-content: center;align-items: center;}
     .item {margin: auto;}
 </style>
+<script
+  src="https://code.jquery.com/jquery-3.7.1.min.js"
+  integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+  crossorigin="anonymous">
+</script>
+<script>
+
+$(document).ready(function () {
+	$("button").click(function () {
+		var classes = [];
+
+		$("button").each(function () {
+			if ($(this).is(".active")) { classes.push($(this).text()); }
+		});
+
+		if (classes.length == $("button").length) {
+			$("button").each(function () {
+				$(this).removeClass("active");
+			});
+
+			classes = [];
+		}
+
+		if (classes.length == 0) { // if no filters selected, show all items
+			$("#sortTable tbody tr").show();
+		} else { // otherwise, hide everything...
+			$("#sortTable tbody tr").hide();
+
+			$("#sortTable tr").each(function () {
+				var show = false;
+				var row = $(this);
+				classes.forEach(function (className) {
+					if (row.find('td').eq(1).text() == className) { show = true; }
+				});
+				if (show) { row.show(); }
+			});
+		}
+	});
+});
+
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("sortTable");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+</script>
 </body>
 </html>
 {{ end }}
